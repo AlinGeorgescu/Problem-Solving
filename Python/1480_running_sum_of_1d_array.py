ace26e7bed7599de4ead7278bf13ac51a6903d93
AlinGeorgescu <alinandrei2007@yahoo.com>
2021-02-01 17:08:45 +0200

    Add problem

---
+++ Python/1480_running_sum_of_1d_array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def main(nums: List[int]) -> List[int]:
+        runningSum = []
+        sum = 0
+
+        for x in nums:
+                sum += x
+                runningSum.append(sum)
+
+        return runningSum
+
+if __name__ == "__main__":
+    n = int(input("Enter number of elements: "))
+    a = list(map(int, input("\nEnter the numbers: ").strip().split()))[:n]
+
+    print(main(a))
